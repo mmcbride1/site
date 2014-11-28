a394f7d0cfe1b928236d4f05fe5a7ac28eb43b73
mmcbride1 <mrmcbride@smcm.edu>
2014-11-27 21:47:23 -0500

    removed window.js

--- window.js
+++
@@ -1,5 +0,0 @@
-function openRequestedPopup()
-{
-window.open('http://www.wontblinkbox.com/window.php','1414203501136','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');
-return false;
-}
