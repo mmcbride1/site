d06a8b5b7da6791e5b8403e8e4b217316e05fabb
mmcbride1 <mrmcbride@smcm.edu>
2015-06-19 22:09:08 -0400

    login page

---
+++ includes/login.php
@@ -0,0 +1,31 @@
+<div id="loginbox" style="font-family: 'Play', sans-serif">
+
+<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC" style="outline: 1px solid #9900CC">
+<tr>
+<form name="login-form" method="post" action="confirmlogin.php">
+<td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr>
+<td colspan="3" bgcolor="#FFFF99" style="outline: 1px solid black"><strong><font color="#9900CC">Log In</font></strong></td>
+</tr>
+<tr>
+<td width="78">username</td>
+<td width="6">:</td>
+<td width="294"><input name="name" type="text" id="name"></td>
+</tr>
+<tr>
+<td width="78">password</td>
+<td width="6">:</td>
+<td width="294"><input name="password" type="password" id="password"></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Login"></td>
+</tr>
+</table>
+</td>
+</form>
+</tr>
+</table>
+</div>
--- loginlanding.php
+++ loginlanding.php
@@ -1,1 +1,31 @@
-<h2>LOGIN UNDER CONSTRUCTION</h2>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<title>Login</title>
+
+<link rel="stylesheet" type="text/css" href="register.css" media="screen" />
+
+</head>
+
+   <body>
+
+      <div id="wrapper">
+
+      <?php include('includes/header.php'); ?>
+
+      <?php include('includes/nav.php'); ?>
+
+      <div id="content">
+
+      <?php include('includes/login.php'); ?>
+
+      </div><!-- end #content -->
+
+      <?php include('includes/footer.php'); ?>
+
+      </div><!-- end #wrapper -->
+
+   </body>
+
+</html>
