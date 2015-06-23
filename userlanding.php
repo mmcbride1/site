0a582a17daed69d8cc83aea46101c7cd9026cd49
mmcbride1 <mrmcbride@smcm.edu>
2015-06-22 20:49:21 -0400

    user landing page essentials

--- userlanding.php
+++ userlanding.php
@@ -1,1 +1,34 @@
-<h2>USER LANDING UNDER CONSTRUCTION!</h2>
+<?php session_start() ?>
+
+<html>
+
+   <head>
+
+      <link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+
+      <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+      <title>Landing</title>
+
+   </head>
+
+   <body>
+
+   <div id="wrapper">
+
+      <?php include('includes/header.php') ?>
+      <?php include('includes/nav.php') ?>
+
+   <div id="content">
+
+      <h2>USER LANDING UNDER CONSTRUCTION!</h2>
+
+   </div>
+
+      <?php include('includes/footer.php') ?>
+
+   </div>
+
+   </body>
+
+</html>
