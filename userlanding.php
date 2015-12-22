8d9e678c5c2f5a18653ab001724f69ba913160e8
mmcbride1 <mrmcbride@smcm.edu>
2015-12-22 00:37:30 -0500

    changes to userlanding.php

--- userlanding.php
+++ userlanding.php
@@ -1,7 +1,7 @@
 <?php
 
    session_start();
-   
+
    include('useroption.php');
    
    /**
@@ -9,11 +9,11 @@
     * username
     *
     **/
-   
+
    function username() {
-   
+
       return $_SESSION['username'];
-   
+
    }
    
    /**
@@ -21,13 +21,13 @@
     * to user
     *
     **/
-   
-   function usergreeting() {
-   
-      $name = username();
-      
+    
+    function usergreeting() {
+
+      $name = (string)username();
+
       return "Welcome $name!";
-   
+
    }
    
    /**
@@ -37,40 +37,41 @@
     *
     **/
     
-   function addsiteform($obj) {
+    function addsiteform($obj) {
 
       include('variables/variables.php');
-   
+
       echo "<div id='add-container'>";
       echo "<form id='add-site' method='post'>";
       echo "<br>";
       echo "<h3>ADD ADDRESS</h3><br>";
       echo "<input type='text' name='new-site'>";
+      echo "<br></br>";
       echo "<input type='submit' value='add'>";
       echo "</form>";
-      
+
       if(!empty($_POST['new-site'])) {
-      
+
          if($obj > 0) {
-         
+
             echo '<h3>';
             echo '<script>';
             echo 'document.write("'.$invalid2.'");';
             echo '</script>';
-            echo '</h3>'; 
-         
+            echo '</h3>';
+
          }
-         
+
          else {
-         
+
             header("location:userlanding.php");
-         
-         }      
-      
+
+         }
+
       }
-      
+
       echo "</div>";
-      
+
    }
 
 ?>
@@ -95,90 +96,100 @@
    
    <body>
    
-      <div id="wrapper">
+      <div id="landing-outer-wrapper">
       
          <?php
-      
+
             include('includes/header.php');
-         
+
             include('includes/nav.php');
-         
-            $option = new UserOption(username());  
-      
+
+            $option = new UserOption(username());
+
          ?>
-      
-         <div id="landing-content">
          
-            <!-- user greeting -->
+         <div id="landing-wrapper">
          
-            <div id="greeting">
+            <div id="landing-content">
             
-               <h2><?php echo usergreeting() ?></h2>
-            
-            </div>
-            
-            <!-- user add and delete site options -->
-            
-            <?php
-            
-               $option->displaysite();
+               <!-- user greeting -->
+
+               <div id="greeting">
                
-               $cnt = $option->sitecount();
+                  <h2><?php echo usergreeting() ?></h2>
+
+                  <h4>Your Account Information</h4>
                
-               if($cnt > 1 && $cnt < 5) {
+               </div>
                
-                  include('includes/checkboxform.php');
-                  
-                  addsiteform($option->addsite($_POST['new-site']));
-               
-               }
-               
-               else if($cnt > 1 && $cnt >= 5) {
-               
-                  include('includes/checkboxform.php');
-               
-               }
-               
-               else {
-               
-                  echo $option->echonlysite();
-                  
-                  addsiteform($option->addsite($_POST['new-site']));
-               
-               }            
-            
+               <?php
+
+                  $option->displaysite();
+
+                  $cnt = $option->sitecount();
+
+                  if($cnt > 1 && $cnt < 5) {
+
+                     include('includes/checkboxform.php');
+
+                     addsiteform($option->addsite($_POST['new-site']));
+
+                  }
+
+                  else if($cnt > 1 && $cnt >= 5) {
+
+                     include('includes/checkboxform.php');
+
+                  }
+
+                  else {
+
+                     include('includes/checkboxform.php');
+
+                     addsiteform($option->addsite($_POST['new-site']));
+
+                  }
+
             ?>
             
             <!-- account deactivation/activation -->
+
+               <div id="accountstate">
+
+                  <h3>Account Status</h3>
+
+                  <?php if($option->status() == 0) { ?>
+
+                  <div id="currentstate"><h4><?php echo $inactive ?></h4></div>
+                  
+                  <br>
+                  
+                  <input type="button" id="opt" value="activate">
+
+                  <?php } else { ?>
+
+                  <div id="currentstate"><h4><?php echo $active ?></h4></div>
+                  
+                  <br>
+                  
+                  <input type="button" id="opt" value="deactivate">
+
+                  <?php } ?>
+
+                  <p>* Deactivation suspends monitroing services.<br>* Reactivate at any time.</p>
+
+               </div>
             
-            <div id="accountstate">
-            
-               <h3>Account Status</h3>
-               
-               <?php if($option->status() == 0) { ?>
-               
-               <div id="currentstate"><?php echo $inactive ?></div>
-               
-               <input type="button" id="opt" value="activate">
-               
-               <?php } else { ?>
-               
-               <div id="currentstate"><?php echo $active ?></div>
-               
-               <input type="button" id="opt" value="deactivate">
-               
-               <?php } ?>
-            
-            </div>   
-      
+            </div>
+         
          </div>
          
          <?php include('includes/footer.php') ?>
-       
-      </div>
       
-   </body>
+      </div>
    
+   </body>
+
 </html>
 
 <!-- remove user site action -->
@@ -186,15 +197,15 @@
 <?php
 
    /* collect sites for deletion */
-   
+
    $list = $_POST['delete_list'];
 
    if(!empty($list)) {
-   
+
       $option->removesite($list);
-      
+
       header("location:userlanding.php");
-   
+
    }
-   
-?>  
+
+?>
